b820d13706451eae5666f3f88fc29f6c9f1fcd38
unknown <yurijlitviniii@gmail.com>
2023-01-10 00:16:04 +0200

    Add Mission component

---
+++ src/components/mission/Mission.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Mission() {
+    return (
+        <div id="mission" className="container">
+            <h1>Mission will be here</h1>
+        </div>
+    );
+}
